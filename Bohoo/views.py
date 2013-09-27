4769e0e42468c8eea0e490d2d1213fc682067322
mutoulbj <flt0426@gmail.com>
2013-09-28 01:39:08 +0800

    修改首页样式和确定默认定位

--- Bohoo/views.py
+++ Bohoo/views.py
@@ -21,9 +21,11 @@ def index(request):
         try:
             groups = Group.objects.filter(category__id=request.GET["c_id"]).order_by("-create_time")[:30]
         except:
-            groups = Group.objects.filter(category__id=1).order_by("-create_time")[:30]
+            groups = Group.objects.filter(category__name="互联网/电子商务").order_by("-create_time")[:30]
     recent_topics = Topic.objects.all().order_by("-create_time")[:5]
     categories = Category.objects.filter(parent__isnull=True)  # 顶级分类
+    init_ca_id = Category.objects.get(name="互联网/电子商务").id   #初始化的分类的id
+    init_ca_parent_id = Category.objects.get(name="互联网/电子商务").parent.id
     vt = loader.get_template("index.html")
     c = RequestContext(
         request, {
@@ -31,6 +33,8 @@ def index(request):
             'recent_topics': recent_topics,
             'categories': categories,
             'groups': groups,
+            'init_ca_id': init_ca_id,
+            'init_ca_parent_id': init_ca_parent_id,
         }
     )
     return HttpResponse(vt.render(c))
