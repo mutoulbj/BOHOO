041ede53818da33adfb744b6c7e626c2c6c54301
mutoulbj <flt0426@gmail.com>
2013-11-10 01:14:24 +0800

    修改！！

--- User/urls.py
+++ User/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns(
     'User.views',
     url(r'^edit/$', 'edit', name='profile_edit'),
     url(r'^profile/(?P<tid>\d+)/$', 'view_profile', name='profile_view'),
+    url(r'^info/(?P<tid>\d+)/$', 'view_info', name='info_view'),
     url(r'^base_info/$', 'base_info_edit', name='base_info_edit')
 )
--- User/views.py
+++ User/views.py
@@ -2,11 +2,10 @@
 # -*- coding:utf-8 -*-
 import json
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.template import loader, RequestContext
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 
 from User.models import MyUser
 from User.forms import UserInfo
@@ -38,6 +37,21 @@ def view_profile(request, tid):
     """
     user = MyUser.objects.get(id=tid)
     vt = loader.get_template("people/profile.html")
+    c = RequestContext(
+        request, {
+            'user': user
+        }
+    )
+    return HttpResponse(vt.render(c))
+
+
+def view_info(request, tid):
+    """
+    查看他人资料
+    @fanlintao
+    """
+    user = get_object_or_404(MyUser, id=tid)
+    vt = loader.get_template("people/info.html")
     c = RequestContext(
         request, {
             'user': user
