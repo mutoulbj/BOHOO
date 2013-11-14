0694ca3b7c12b5beb12028fb633886e58fb99122
mutoulbj <flt0426@gmail.com>
2013-11-14 23:41:29 +0800

    design

--- User/views.py
+++ User/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import redirect, get_object_or_404
 
 from User.models import MyUser
 from User.forms import UserInfo
+from friends.models import Friendship
 
 
 @login_required()
@@ -51,11 +52,15 @@ def view_info(request, tid):
     @fanlintao
     """
     t_user = get_object_or_404(MyUser, id=tid)
-    print t_user.username
+    is_followed = False
+    f_list = Friendship.objects.filter(from_user=request.user, to_user=t_user).values_list('from_user', flat=True)
+    if request.user.id in f_list:
+        is_followed = True
     vt = loader.get_template("people/info.html")
     c = RequestContext(
         request, {
-            't_user': t_user
+            't_user': t_user,
+            'is_followed': is_followed
         }
     )
     return HttpResponse(vt.render(c))
