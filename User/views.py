b833a6b50ddf73d3beaa5db860f9743c5bdb6b3a
mutoulbj <flt0426@gmail.com>
2013-11-19 22:41:32 +0800

    update follows and member info

--- User/views.py
+++ User/views.py
@@ -6,6 +6,7 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.template import loader, RequestContext
 from django.shortcuts import redirect, get_object_or_404
+from django.utils.datastructures import MultiValueDictKeyError
 from Bohoo.settings import JOINED_GROUPS_NUM, ADD_TOPIC_NUM, REPLY_TOPIC_NUM
 
 from User.models import MyUser
@@ -65,12 +66,23 @@ def view_info(request, tid):
     """
     t_user = get_object_or_404(MyUser, id=tid)
     t_joined_groups = get_groups(t_user)[0:JOINED_GROUPS_NUM:-1]   # 加入的群组
+    active_tab = 'followed'    # 默认激活'关注了'的tab
+
 
     #关注了
     t_followed = Friendship.objects.filter(from_user=t_user)
 
     # 关注者
     t_follower = Friendship.objects.filter(to_user=t_user)
+
+    try:
+        if request.method == 'GET':
+            f_user = get_object_or_404(MyUser, id=request.GET['follow_user'])
+            t_followed = Friendship.objects.filter(from_user=f_user)
+            t_follower = Friendship.objects.filter(to_user=f_user)
+            active_tab = request.GET['follow_type']
+    except MultiValueDictKeyError:
+        pass
 
     # 是否已经关注
     is_followed = False
@@ -88,7 +100,8 @@ def view_info(request, tid):
             'is_followed': is_followed,
             'joined_groups': t_joined_groups,
             'followed': t_followed,
-            'follower': t_follower
+            'follower': t_follower,
+            'active_tab': active_tab
         }
     )
     return HttpResponse(vt.render(c))
@@ -102,11 +115,23 @@ def m_add_topic(request, tid):
     t_user = get_object_or_404(MyUser, id=tid)
     t_add_topic = Topic.objects.filter(creator=t_user).order_by('-create_time')[0:ADD_TOPIC_NUM]   # 添加的话题
 
+    active_tab = 'followed'    # 默认激活'关注了'的tab
+
+
     #关注了
     t_followed = Friendship.objects.filter(from_user=t_user)
 
     # 关注者
     t_follower = Friendship.objects.filter(to_user=t_user)
+
+    try:
+        if request.method == 'GET':
+            f_user = get_object_or_404(MyUser, id=request.GET['follow_user'])
+            t_followed = Friendship.objects.filter(from_user=f_user)
+            t_follower = Friendship.objects.filter(to_user=f_user)
+            active_tab = request.GET['follow_type']
+    except MultiValueDictKeyError:
+        pass
 
     # 是否已经关注
     is_followed = False
@@ -124,7 +149,8 @@ def m_add_topic(request, tid):
             'is_followed': is_followed,
             'add_topics': t_add_topic,
             'followed': t_followed,
-            'follower': t_follower
+            'follower': t_follower,
+            'active_tab': active_tab
         }
     )
     return HttpResponse(vt.render(c))
@@ -139,12 +165,23 @@ def m_reply_topic(request, tid):
     t_r_topic_ids = Reply.objects.filter(creator=t_user, reply__isnull=True,
                                          topic__isnull=False).values_list('topic', flat=True)
     t_reply_topics = Topic.objects.filter(id__in=list(set(t_r_topic_ids))).order_by('-create_time')[0:REPLY_TOPIC_NUM]
+    active_tab = 'followed'    # 默认激活'关注了'的tab
+
 
     #关注了
     t_followed = Friendship.objects.filter(from_user=t_user)
 
     # 关注者
     t_follower = Friendship.objects.filter(to_user=t_user)
+
+    try:
+        if request.method == 'GET':
+            f_user = get_object_or_404(MyUser, id=request.GET['follow_user'])
+            t_followed = Friendship.objects.filter(from_user=f_user)
+            t_follower = Friendship.objects.filter(to_user=f_user)
+            active_tab = request.GET['follow_type']
+    except MultiValueDictKeyError:
+        pass
 
     # 是否已经关注
     is_followed = False
@@ -162,7 +199,8 @@ def m_reply_topic(request, tid):
             'is_followed': is_followed,
             'reply_topics': t_reply_topics,
             'followed': t_followed,
-            'follower': t_follower
+            'follower': t_follower,
+            'active_tab': active_tab
         }
     )
     return HttpResponse(vt.render(c))
